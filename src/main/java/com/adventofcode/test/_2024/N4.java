020613cc8119f0eb87826cb5219a138899a9cc98
Jonathan Pallini <jpallini@yahoo.it>
2024-12-05 23:22:09 +0100

    2024 day 4 part 2

--- src/main/java/com/adventofcode/test/_2024/N4.java
+++ src/main/java/com/adventofcode/test/_2024/N4.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 public class N4 {
@@ -19,8 +18,10 @@ public class N4 {
             FileReader fileReader = new FileReader(filePath);
             try (BufferedReader reader = new BufferedReader(fileReader)) {
                 List<String> lines = reader.lines().toList();
-                int part1 = part1(lines);
-                System.out.println("Part 1: " + part1);
+//                int part1 = part1(lines);
+//                System.out.println("Part 1: " + part1);
+                int part2 = part2(lines);
+                System.out.println("Part 2: " + part2);
             } catch (FileNotFoundException f) {
                 System.out.println(filePath + " does not exist");
             } catch (IOException e) {
@@ -54,7 +55,7 @@ public class N4 {
             }
         }
 
-        System.out.println("Matrix " + Arrays.deepToString(matrix));
+//        System.out.println("Matrix " + Arrays.deepToString(matrix));
         System.out.println("Sum p1: " + sum);
         return sum;
     }
@@ -84,7 +85,7 @@ public class N4 {
         int n = word.length();
         int x = i;
         int y = j;
-        
+
         if (x + (n - 1) * di < 0 || x + (n - 1) * di >= matrix.length ||
                 y + (n - 1) * dj < 0 || y + (n - 1) * dj >= matrix[0].length) {
             return false;
@@ -98,6 +99,53 @@ public class N4 {
             y += dj;
         }
         return true;
+    }
+
+    public static int part2(List<String> lines) {
+        int sum = 0;
+        int matrixWidth = lines.get(0).length();
+        int matrixHeight = lines.size();
+
+        char[][] matrix = new char[matrixHeight][matrixWidth];
+
+        // Creating matrix
+        for (int i = 0; i < matrixHeight; i++) {
+            for (int j = 0; j < matrixWidth; j++) {
+                matrix[i][j] = lines.get(i).toCharArray()[j];
+            }
+        }
+
+        // Finding the X
+        for (int i = 0; i < matrixHeight; i++) {
+            for (int j = 0; j < matrixWidth; j++) {
+                if (matrix[i][j] == 'A') {
+                    sum = findX(matrix, i, j, sum);
+                }
+            }
+        }
+
+//        System.out.println("Matrix " + Arrays.deepToString(matrix));
+//        System.out.println("Sum p2: " + sum);
+        return sum;
+    }
+
+    private static int findX(char[][] matrix, int i, int j, int sum) {
+        if (i > 0 && j > 0 && i + 1 < matrix.length && j + 1 < matrix[0].length) {
+            char rightUp = matrix[i - 1][j + 1];
+            char leftUp = matrix[i - 1][j - 1];
+            char rightDown = matrix[i + 1][j + 1];
+            char leftDown = matrix[i + 1][j - 1];
+
+            if ((rightUp == 'M' && leftUp == 'M' && rightDown == 'S' && leftDown == 'S') ||
+                    (rightUp == 'S' && leftUp == 'S' && rightDown == 'M' && leftDown == 'M') ||
+                    (rightUp == 'S' && leftUp == 'M' && rightDown == 'S' && leftDown == 'M') ||
+                    (rightUp == 'M' && leftUp == 'S' && rightDown == 'M' && leftDown == 'S')) {
+//                System.out.println("Found 'X-MAS' with center at (" + i + ", " + j + ")");
+                sum++;
+            }
+        }
+
+        return sum;
     }
 
 }
