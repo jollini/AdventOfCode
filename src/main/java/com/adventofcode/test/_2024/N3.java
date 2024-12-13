b7b7cf5c548216e737ef5657f25d96054fc997bc
Jonathan Pallini <jpallini@yahoo.it>
2024-12-13 11:33:16 +0100

    2024 day 3 part 2

--- src/main/java/com/adventofcode/test/_2024/N3.java
+++ src/main/java/com/adventofcode/test/_2024/N3.java
@@ -20,7 +20,10 @@ public class N3 {
             FileReader fileReader = new FileReader(filePath);
             try (BufferedReader reader = new BufferedReader(fileReader)) {
                 List<String> lines = reader.lines().toList();
-                part1(lines);
+                int part1 = part1(lines);
+                System.out.println("Part 1: " + part1);
+                int part2 = part2(lines);
+                System.out.println("Part 2: " + part2);
             } catch (FileNotFoundException f) {
                 System.out.println(filePath + " does not exist");
             } catch (IOException e) {
@@ -42,7 +45,33 @@ public class N3 {
                 sum += Integer.parseInt(stringNumber1) * Integer.parseInt(stringNumber2);
             }
         }
-        System.out.println("Sum p1: " + sum);
+//        System.out.println("Sum p1: " + sum);
+        return sum;
+    }
+
+    public static int part2(List<String> lines) {
+        Pattern regex = Pattern.compile("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)");
+        int sum = 0;
+        boolean valid = true;
+
+        for (String l : lines) {
+            Matcher firstMatcher = regex.matcher(l);
+
+            while (firstMatcher.find()) {
+                if (firstMatcher.group().contains("do()")) {
+                    valid = true;
+                } else if (firstMatcher.group().contains("don't()")) {
+                    valid = false;
+                }
+
+                String stringNumber1 = firstMatcher.group(1);
+                String stringNumber2 = firstMatcher.group(2);
+                if (valid && stringNumber1 != null && stringNumber2 != null) {
+                    sum += Integer.parseInt(stringNumber1) * Integer.parseInt(stringNumber2);
+                }
+            }
+        }
+//        System.out.println("Sum p2: " + sum);
         return sum;
     }
 }
